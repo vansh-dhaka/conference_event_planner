1a3b04cf3a7425c58bb78d916c11959b2d5b8e18
vansh-dhaka <27vanshdhaka@gmail.com>
2026-03-29 10:04:32 -0400

    Display selected items in a table

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -57,13 +57,72 @@ const ConferenceEvent = () => {
 
     const getItemsFromTotalCost = () => {
         const items = [];
-    };
+        venueItems.forEach((item) => {
+          if (item.quantity > 0) {
+            items.push({ ...item, type: "venue" });
+          }
+        });
+        avItems.forEach((item) => {
+          if (
+            item.quantity > 0 &&
+            !items.some((i) => i.name === item.name && i.type === "av")
+          ) {
+            items.push({ ...item, type: "av" });
+          }
+        });
+        mealsItems.forEach((item) => {
+          if (item.selected) {
+            const itemForDisplay = { ...item, type: "meals" };
+            if (item.numberOfPeople) {
+              itemForDisplay.numberOfPeople = numberOfPeople;
+            }
+            items.push(itemForDisplay);
+          }
+        });
+        return items;
+      };   
 
     const items = getItemsFromTotalCost();
 
     const ItemsDisplay = ({ items }) => {
-
+        const ItemsDisplay = ({ items }) => {
+            console.log(items);
+            return <>
+                <div className="display_box1">
+                    {items.length === 0 && <p>No items selected</p>}
+                    <table className="table_item_data">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Unit Cost</th>
+                                <th>Quantity</th>
+                                <th>Subtotal</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {items.map((item, index) => (
+                                <tr key={index}>
+                                    <td>{item.name}</td>
+                                    <td>${item.cost}</td>
+                                    <td>
+                                        {item.type === "meals" || item.numberOfPeople
+                                        ? ` For ${numberOfPeople} people`
+                                        : item.quantity}
+                                    </td>
+                                    <td>{item.type === "meals" || item.numberOfPeople
+                                        ? `${item.cost * numberOfPeople}`
+                                        : `${item.cost * item.quantity}`}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </>
+        };
+        
     };
+    
     const calculateTotalCost = (section) => {
         let totalCost = 0;
         if (section === "venue") {
@@ -74,18 +133,19 @@ const ConferenceEvent = () => {
             avItems.forEach((item) => {
                 totalCost += item.cost * item.quantity;
             })
-        } else if ( section == "meals" {
+        } else if ( section == "meals") {
             mealsItems.forEach((item) => {
                 if (item.selected) {
                     totalCost += item.cost * numberOfPeople;
                 }
             })
-        })
+        }
         return totalCost;
       };
     const venueTotalCost = calculateTotalCost("venue");
     const avTotalCost = calculateTotalCost("av");
-    const mealsTotalCost = calculateTotalCost("meals";)
+    const mealsTotalCost = calculateTotalCost("meals");
+    
 
     const navigateToProducts = (idType) => {
         if (idType == '#venue' || idType == '#addons' || idType == '#meals') {
@@ -95,6 +155,15 @@ const ConferenceEvent = () => {
         }
       }
 
+      const totalCosts = {
+        venue: venueTotalCost,
+        av: avTotalCost,
+        meals: mealsTotalCost,
+    };
+
+
+    
+    
     return (
         <>
             <navbar className="navbar_event_conference">
